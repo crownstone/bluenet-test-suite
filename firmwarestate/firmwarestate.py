dae683fac3e0eb058d9d4d27d447500986adcf2f
Arend de Jonge <arenddejonge@gmail.com>
2020-01-16 18:45:03 +0100

    get classname from gnu pretty_function

--- firmwarestate/firmwarestate.py
+++ firmwarestate/firmwarestate.py
@@ -10,6 +10,7 @@ from BluenetLib.lib.protocol.BluenetTypes import ControlType
 from BluenetLib.lib.topics.SystemTopics import SystemTopics
 from BluenetLib.lib.util.Conversion import Conversion
 
+import pprint
 import pygame #for nice keyboard input
 
 # def sendToCrownstone(commandtype, packetcontent):
@@ -35,6 +36,9 @@ class FirmwareState:
     """
     def __init__(self):
         BluenetEventBus.subscribe(SystemTopics.uartNewPackage, self.parse)
+        # statedict: dict (int -> dict (string -> value) ),
+        # thisptr -> valuename -> value
+        self.statedict = dict()
 
     def parse(self, dataPacket):
         opCode = dataPacket.opCode
@@ -43,15 +47,49 @@ class FirmwareState:
             for byte in dataPacket.payload:
                 if byte < 128:
                     stringResult += chr(byte)
-            logStr = "FW update: %15.3f - %s" % (time.time(), stringResult)
-            sys.stdout.write(logStr)
+            statelist = stringResult.split(",")
 
+            self.pushvalue("0x"+statelist[0], statelist[1], statelist[2], statelist[3])
+
+    def construct(self, ptr, typename):
+        """
+        Appends [typename] to self.statedict[ptr]["typename."].
+        """
+        if ptr not in self.statedict:
+            self.statedict[ptr] = dict()
+            self.statedict[ptr]["typename"] = ""
+        self.statedict[ptr]["typename"] += typename
+
+    def destruct(self, ptr):
+        """
+        Removes the object with address [ptr] from [self.statedict]
+        """
+        self.statedict.pop(ptr, None)
+
+    def pushvalue(self, ptr, scopename, valuename, value):
+        """
+        Add or update value in the state dict. If ptr wasn't contained in it yet, adds an entry
+        """
+        if ptr not in self.statedict:
+            classname = ""
+            try:
+                # try to infer class name from gnu __PRETTY_FUNCTION__ (impl dependent).
+                # it should be the last cpp token of the top-most level scoped part in the string
+                # (note: assumes our code is in global namespace.)
+                # (note: it's not possible to distinguish this from top-level function names.)
+                classname = scopename.split("::")[0].split(" ")[-1]
+            except Exception as inst:
+                # the exception instance
+                print("couldn't identify classname: " + type(inst) + " " + scopename)
+                pass
+            self.construct(ptr, classname)
+
+        self.statedict[ptr][valuename] = value
 
 class Main:
     """
     If this file is run as stand alone script, it will output the current state of the firmware.
     """
-    #construction
     def __init__(self):
         # Create new instance of Bluenet
         self.bluenet = Bluenet()
@@ -86,6 +124,8 @@ class Main:
     def run(self):
         print("firmwarestate up and running")
 
+        prettyprinter = pprint.PrettyPrinter(indent=4)
+        prettyprint = prettyprinter.pprint
         run = True
         while run:
             pygame.time.delay(100)
@@ -93,6 +133,9 @@ class Main:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == ord(' '):
+                        prettyprint(self.fwState.statedict)
 
 if __name__ == "__main__":
     with Main() as m:

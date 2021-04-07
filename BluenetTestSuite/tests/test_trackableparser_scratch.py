415bc0aba9af2310d9d5f405de8ccc979df48266
Arend de Jonge <arenddejonge@gmail.com>
2021-04-07 16:04:00 +0200

    clean up the trackable parser upload script

--- BluenetTestSuite/tests/test_trackableparser_scratch.py
+++ BluenetTestSuite/tests/test_trackableparser_scratch.py
@@ -3,46 +3,77 @@ Just a scratch that loads a cuckoo filter into the firmware in sevarl chunks.
 """
 import time
 
-from BluenetTestSuite.firmwarecontrol.behaviourstore import sendBehaviour, buildTwilight, sendClearBehaviourStoreEvent
-from crownstone_core.protocol.BluenetTypes import ControlType
 from crownstone_core.packets.TrackableParser.TrackableParserPackets import *
 from crownstone_core.packets.TrackableParser.TrackableParserCommands import *
-
-
 from crownstone_core.util.cuckoofilter import *
 
 from crownstone_uart import CrownstoneUart, UartEventBus
-from BluenetTestSuite.firmwarecontrol.datatransport import sendCommandToCrownstone, sendEventToCrownstone
+from crownstone_uart.topics.SystemTopics import SystemTopics
+
+from BluenetTestSuite.firmwarecontrol.datatransport import sendEventToCrownstone, sendCommandToCrownstone
 from BluenetTestSuite.firmwarecontrol.InternalEventCodes import *
 
 from bluenet_logs import BluenetLogs
-from crownstone_uart.topics.SystemTopics import SystemTopics
+
+# --------------------------
+# setup uart stuff
+# --------------------------
 
 bluenetLogs = BluenetLogs()
 bluenetLogs.setSourceFilesDir("/home/arend/Documents/crownstone-bluenet/bluenet/source")
 
-# construct cuckoo filter
+def successhandler(*args):
+    print("success handler called", *args)
+
+def failhandler(*args):
+    print("fail handler called", *args)
+
+uartfail = UartEventBus.subscribe(SystemTopics.uartWriteError,failhandler)
+uartsucces = UartEventBus.subscribe(SystemTopics.uartWriteSuccess, successhandler)
+
+uart = CrownstoneUart()
+uart.initialize_usb_sync(port='/dev/ttyACM0')
+time.sleep(10)
+
+# --------------------------
+# construct tracking filter
+# --------------------------
+
+trackingfilter = TrackingFilterData()
+
+# ---------------------------
+# tracking filter cuckoo data
+# ---------------------------
+
 cuckoo = CuckooFilter(3, 2)
 some_element = [i % 0x100 for i in range(10)]
 cuckoo.add(some_element)
 
+trackingfilter.filter = cuckoo.getData()
+
 print("filter size: ", cuckoo.size())
 print(cuckoo.contains(some_element))
 
-# construct tracking filter
-trackingfilter = TrackingFilterData()
+# ------------------------
+# tracking filter metadata
+# ------------------------
+
 trackingfilter.metadata.protocol = 17
 trackingfilter.metadata.version = 4567
 trackingfilter.metadata.profileId = 0xae
 trackingfilter.metadata.inputType = FilterInputType.MacAddress
 trackingfilter.metadata.flags = 0b10101010
-trackingfilter.filter = cuckoo.getData()
 
 print("metadata", len(trackingfilter.metadata.getPacket()), trackingfilter.metadata.getPacket())
 print("filter  ", len(trackingfilter.filter.getPacket()), trackingfilter.filter.getPacket())
 print("combined", len(trackingfilter.getPacket()), trackingfilter.getPacket())
 
+# -------------------
+# test infrastructure
+# -------------------
+
 def upload(trackingfilter, max_chunk_size):
+    print(" ** starting upload **")
     filter_bytes = trackingfilter.getPacket()
     data_len = len(filter_bytes)
     for start_index in range(0, data_len, max_chunk_size):
@@ -61,27 +92,18 @@ def upload(trackingfilter, max_chunk_size):
         time.sleep(1)
         # sendCommandToCrownstone(ControlType.TRACKABLE_PARSER_UPLOAD_FILTER, upload_packet.getPacket())
 
-def successhandler(*args):
-    print("success handler called", *args)
 
-def failhandler(*args):
-    print("fail handler called", *args)
+# ----------------------------
+# execute the upload procedure
+# ----------------------------
 
-uartfail = UartEventBus.subscribe(SystemTopics.uartWriteError,failhandler)
-uartsucces = UartEventBus.subscribe(SystemTopics.uartWriteSuccess, successhandler)
-
-uart = CrownstoneUart()
-
-uart.initialize_usb_sync(port='/dev/ttyACM0')
-print(" ** starting upload **")
-
-print("---------------------------------")
-time.sleep(10)
+print("------------- upload filter ---------------")
 upload(trackingfilter, max_chunk_size=10)
 
-print("---------------------------------")
+print("------------- commit filter ---------------")
 commitCommand = CommitFilterChangesCommandPacket()
 sendEventToCrownstone(EventType.CMD_COMMIT_FILTER_CHANGES, commitCommand.getPacket())
+
 print("---------------------------------")
 time.sleep(30)
 uart.stop()

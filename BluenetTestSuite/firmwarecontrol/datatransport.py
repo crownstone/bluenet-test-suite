7f7fdf3c34eebd8f2bab7ea8d2ac93e68b68ae22
Arend de Jonge <arenddejonge@gmail.com>
2021-04-07 11:58:38 +0200

    fix inject event function

--- BluenetTestSuite/firmwarecontrol/datatransport.py
+++ BluenetTestSuite/firmwarecontrol/datatransport.py
@@ -3,8 +3,7 @@ from crownstone_core.protocol.BlePackets import ControlPacket
 
 from crownstone_uart.core.UartEventBus import UartEventBus
 from crownstone_uart.core.uart.uartPackets.UartWrapperPacket import UartWrapperPacket
-
-from crownstone_uart.core.uart.UartTypes import UartTxType
+from crownstone_uart.core.uart.UartTypes import UartTxType, UartMessageType
 from crownstone_uart.topics.SystemTopics import SystemTopics
 
 import time
@@ -12,17 +11,18 @@ import time
 def sleepAfterUartCommand():
     time.sleep(1)
 
-def sendEventToCrownstone(eventtype, eventdata):
+def sendEventToCrownstone(cs_event_type, cs_event_data):
     """
     To send events over the firmware internal event bus use this method.
     eventtype: CS_TYPE
     eventdata: corresponds to eventtype.
     """
-    payload = []
-    payload += Conversion.uint16_to_uint8_array(eventtype)
-    payload += eventdata
-    uartPacket = UartWrapperPacket(UartTxType.MOCK_INTERNAL_EVT,payload).getPacket()
-    UartEventBus.emit(SystemTopics.uartWriteData, uartPacket)
+    uart_message = []
+    uart_message += Conversion.uint16_to_uint8_array(UartTxType.MOCK_INTERNAL_EVT)
+    uart_message += Conversion.uint16_to_uint8_array(cs_event_type)
+    uart_message += cs_event_data
+    uart_warpper_packet = UartWrapperPacket(UartMessageType.UART_MESSAGE,uart_message).getPacket()
+    UartEventBus.emit(SystemTopics.uartWriteData, uart_warpper_packet)
     sleepAfterUartCommand()
 
 def sendCommandToCrownstone(commandtype, packetcontent):

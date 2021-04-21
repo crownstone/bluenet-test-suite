f252003469f8f96015f0e83ad7b40cbd0cd32fb5
Arend de Jonge <arenddejonge@gmail.com>
2021-04-21 14:37:29 +0200

    expand test to test the commit filters command

--- BluenetTestSuite/tests/test_trackableparser_scratch.py
+++ BluenetTestSuite/tests/test_trackableparser_scratch.py
@@ -4,8 +4,13 @@ Just a scratch that loads a cuckoo filter into the firmware in sevarl chunks.
 import time
 
 from crownstone_core.packets.TrackableParser.TrackableParserPackets import *
-from crownstone_core.packets.TrackableParser.TrackableParserCommands import *
+from crownstone_core.packets.TrackableParser.TrackableParserCommands import RemoveFilterCommandPacket
+from crownstone_core.packets.TrackableParser.TrackableParserCommands import UploadFilterCommandPacket
+from crownstone_core.packets.TrackableParser.TrackableParserCommands import CommitFilterChangesCommandPacket
+from crownstone_core.packets.TrackableParser.TrackableParserCommands import GetFilterSummariesCommandPacket
+
 from crownstone_core.util.cuckoofilter import *
+from crownstone_core.util.TrackableParser import MasterCrc, FilterCrc
 
 from crownstone_uart import CrownstoneUart, UartEventBus
 from crownstone_uart.topics.SystemTopics import SystemTopics
@@ -14,6 +19,48 @@ from BluenetTestSuite.firmwarecontrol.datatransport import sendEventToCrownstone
 from BluenetTestSuite.firmwarecontrol.InternalEventCodes import *
 
 from bluenet_logs import BluenetLogs
+
+
+
+# ------------------------------
+# construct tracking filter data
+# ------------------------------
+
+cuckoo = CuckooFilter(3, 2)
+cuckoo.add([0xac, 0x23, 0x3f, 0x71, 0xca, 0x77][::-1])
+
+trackingfilter = TrackingFilterData()
+trackingfilter.filter = cuckoo.getData()
+
+trackingfilter.metadata.protocol = 17
+trackingfilter.metadata.version = 4567
+trackingfilter.metadata.profileId = 0xae
+trackingfilter.metadata.inputType = FilterInputType.MacAddress
+trackingfilter.metadata.flags = 0b10101010
+
+# ---------- another one for testing ----------
+
+cuckoo1 = CuckooFilter(4, 4)
+cuckoo1.add([x for x in range (6)])
+
+trackingfilter1 = TrackingFilterData()
+trackingfilter1.filter = cuckoo.getData()
+
+trackingfilter1.metadata.protocol = 17
+trackingfilter1.metadata.version = 4567
+trackingfilter1.metadata.profileId = 0x1
+trackingfilter1.metadata.inputType = FilterInputType.MacAddress
+trackingfilter1.metadata.flags = 0b10101010
+
+
+# --------------------------
+# MasterCrc
+# --------------------------
+
+masterCrc = MasterCrc([trackingfilter1, trackingfilter]) # be weary of filter id sorting...
+print("filter crc:", FilterCrc(trackingfilter))
+print("filter crc:", FilterCrc(trackingfilter1))
+print("master crc:", masterCrc)
 
 # --------------------------
 # setup uart stuff
@@ -35,45 +82,12 @@ uart = CrownstoneUart()
 uart.initialize_usb_sync(port='/dev/ttyACM0')
 time.sleep(10)
 
-# --------------------------
-# construct tracking filter
-# --------------------------
-
-trackingfilter = TrackingFilterData()
-
-# ---------------------------
-# tracking filter cuckoo data
-# ---------------------------
-
-cuckoo = CuckooFilter(3, 2)
-some_element = [i % 0x100 for i in range(10)]
-cuckoo.add(some_element)
-cuckoo.add([0xe7, 0x9b, 0x2d, 0xb7, 0x64, 0xdb][::-1])
-
-trackingfilter.filter = cuckoo.getData()
-
-print("filter size: ", cuckoo.size())
-print(cuckoo.contains(some_element))
-
-# ------------------------
-# tracking filter metadata
-# ------------------------
-
-trackingfilter.metadata.protocol = 17
-trackingfilter.metadata.version = 4567
-trackingfilter.metadata.profileId = 0xae
-trackingfilter.metadata.inputType = FilterInputType.MacAddress
-trackingfilter.metadata.flags = 0b10101010
-
-print("metadata", len(trackingfilter.metadata.getPacket()), trackingfilter.metadata.getPacket())
-print("filter  ", len(trackingfilter.filter.getPacket()), trackingfilter.filter.getPacket())
-print("combined", len(trackingfilter.getPacket()), trackingfilter.getPacket())
 
 # -------------------
 # test infrastructure
 # -------------------
 
-def upload(trackingfilter, max_chunk_size):
+def upload(trackingfilter, filterId, max_chunk_size):
     print(" ** starting upload **")
     filter_bytes = trackingfilter.getPacket()
     data_len = len(filter_bytes)
@@ -82,7 +96,7 @@ def upload(trackingfilter, max_chunk_size):
         print(" -------- Send packet chunk (EVENT) -------- ", start_index,"-", end_index)
 
         upload_packet = UploadFilterCommandPacket()
-        upload_packet.filterId = 1
+        upload_packet.filterId = filterId
         upload_packet.totalSize = data_len
         upload_packet.chunkSize = end_index - start_index
         upload_packet.chunkStartIndex = start_index
@@ -90,21 +104,64 @@ def upload(trackingfilter, max_chunk_size):
         print([hex(x) for x in upload_packet.getPacket()])
 
         sendEventToCrownstone(EventType.CMD_UPLOAD_FILTER, upload_packet.getPacket())
-        time.sleep(1)
+        time.sleep(0.5)
         # sendCommandToCrownstone(ControlType.TRACKABLE_PARSER_UPLOAD_FILTER, upload_packet.getPacket())
 
+def remove(filterId):
+    removePacket = RemoveFilterCommandPacket(filterId)
+    sendEventToCrownstone(EventType.CMD_REMOVE_FILTER, removePacket.getPacket())
+
+def commit(crc, version):
+    commitCommand = CommitFilterChangesCommandPacket()
+    commitCommand.masterCrc = crc
+    commitCommand.masterVersion = version
+    sendEventToCrownstone(EventType.CMD_COMMIT_FILTER_CHANGES, commitCommand.getPacket())
+
+def getStatus():
+    statusCommand = GetFilterSummariesCommandPacket()
+    sendEventToCrownstone(EventType.CMD_GET_FILTER_SUMMARIES, statusCommand.getPacket())
+
 
 # ----------------------------
-# execute the upload procedure
+# execute some commands
 # ----------------------------
 
-print("------------- upload filter ---------------")
-upload(trackingfilter, max_chunk_size=10)
+# for id in [1,5,4]:
+print("------------- upload filter ---------------");
+upload(trackingfilter, filterId = 1,  max_chunk_size=10)
+time.sleep(2)
 
-print("------------- commit filter ---------------")
-commitCommand = CommitFilterChangesCommandPacket()
-sendEventToCrownstone(EventType.CMD_COMMIT_FILTER_CHANGES, commitCommand.getPacket())
+print("------------- upload filter ---------------");
+upload(trackingfilter1, filterId = 0,  max_chunk_size=10)
+time.sleep(2)
 
-print("---------------------------------")
-time.sleep(30)
+print("------------- commit upload ---------------");
+commit(crc = masterCrc, version = 1)
+time.sleep(5)
+
+# getStatus()
+#
+# for id in [4, 1, 5]:
+#     print("------------- upload filter ---------------");
+#     upload(trackingfilter, filterId=id, max_chunk_size=10)
+#     time.sleep(2)
+#
+#     print("------------- commit upload ---------------");
+#     commit()
+#     time.sleep(5)
+#
+# getStatus()
+#
+# for id in [5, 6]:
+#     print("--------------- remove filter ------------------")
+#     remove(filterId=id)
+#     time.sleep(5)
+#
+#     print("---------------commit remove ------------------")
+#     commit()
+#     time.sleep(5)
+
+getStatus()
+
+time.sleep(60)
 uart.stop()
